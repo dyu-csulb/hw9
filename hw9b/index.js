5a00e2bcf8d99b11fe6d3aa7a5e87b454c2d7ef2
buffalosushi817 <44121873+buffalosushi817@users.noreply.github.com>
2021-04-12 00:21:52 -0700

    updated hw

--- hw9b/index.js
+++ hw9b/index.js
@@ -7,8 +7,30 @@ const path = require('path');
 //Declare a variable to use Express
 const app = express();
 
-//Pointing app to reference static files in public folder. 
-app.use(express.static(path.join(__dirname, 'views')));
+
+//Routes
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/views/index.html`);
+});
+
+app.get('/ex1', (req, res) => {
+    res.sendFile(`${__dirname}/views/ex1.html`);
+});
+
+app.get('/ex2', (req, res) => {
+    res.sendFile(`${__dirname}/views/ex2.html`);
+});
+
+app.get('/ex3', (req, res) => {
+    res.sendFile(`${__dirname}/views/ex3.html`);
+});
+
+//Use Static files
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'css')));
+
+
+
 
 //Declare a variable to hold your port and run your server. 
 const PORT = process.env.PORT || 5000;
